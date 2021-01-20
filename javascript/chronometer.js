9e092e06cafabd83cb237b7ff82da5cbdbb0367a
Cindy <cindytvn+git@gmail.com>
2021-01-20 21:47:08 +0100

    It 1 finished. Incl millisec. Jasmine gave green light for it 2

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,66 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0    
   }
+
   startClick(callback) {
-    // ... your code goes here
+      this.intervalID = setInterval(() => {
+      this.currentTime += 1      
+    }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
+  
   getSeconds() {
-    // ... your code goes here
+    //check if you get the correct values
+    // console.log('current time', this.currentTime)
+    // console.log('current time min', this.currentTime/60)   
+    // console.log('current time sec-part', this.currentTime % 60)   
+    return this.currentTime % 60 
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    const tens = Math.floor(this.currentTime / 10);
+    const ones = this.currentTime % 10;
+    const twoDigitNotation = `${tens}${ones}`
+    // console.log('value current time: ', this.currentTime)
+    // console.log('The tens: ', tens)
+    // console.log('The ones: ', ones)
+    // console.log(twoDigitNotation)
+
+    return twoDigitNotation
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalID)
   }
   resetClick() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    let min = Math.floor(this.currentTime/60)
+    let sec = this.currentTime % 60
+    let milli = this.currentTime % 60
+    
+    let splitTime = ''
+    console.log('value current time: ', this.currentTime)
+    console.log('min: ', min)
+    console.log('sec: ', sec)
+    console.log('milli: ', milli)
+
+    let minDigit
+    let secDigit
+    let milliDigit
+    if (min < 10)   {minDigit = `${0}${min}`} 
+             else   {minDigit = `${min}`}
+    if (sec < 10)   {secDigit = `${0}${sec}`} 
+             else   {secDigit = `${sec}`} 
+    if (milli < 10) {milliDigit = `${0}${milli}`} 
+              else  {milliDigit = `${milli}`}
+
+    console.log(`${minDigit}:${secDigit}:${milliDigit}`)
+    //return(`${minDigit}:${secDigit}:${milliDigit}`) // incl millisec
+    return(`${minDigit}:${secDigit}`)
   }
 }
+
